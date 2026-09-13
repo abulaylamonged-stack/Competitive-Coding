874d291b1695d3e741b24cc6021f3dd9a8dc2278
abulaylamonged-stack <abulaylamonged@gmail.com>
2026-09-13 23:17:05 +0530

    Update Exp_1.2

---
+++ Exp_1.2/solution.py
@@ -0,0 +1,18 @@
+# LeetCode 238 - Product of Array Except Self
+
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+
+        return answer
